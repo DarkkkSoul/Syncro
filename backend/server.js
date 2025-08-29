8c047d70fd1301a6a54c68f9ee09aff33e63e37d
Mahesh Kumar G <maheshh.kumar1508@gmail.com>
2025-08-30 01:28:23 +0530

    settingup middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,29 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import errorHandler from './middlewares/errorHandler.middleware.js';
+import cookieParser from 'cookie-parser';
+import authRouter from './routes/auth.route.js';
+import connectToDB from './database/mongo.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use('/api/v1/auth', authRouter)
+
+
+app.use(errorHandler);
+
 app.get('/', (req, res) => {
     res.send('Welcome to the backend of Syncro!');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
+    connectToDB();
 });
