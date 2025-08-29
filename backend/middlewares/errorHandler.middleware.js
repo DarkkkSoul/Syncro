e14f3cda6231f499fd4a463eb3462436536664e1
Mahesh Kumar G <maheshh.kumar1508@gmail.com>
2025-08-29 23:01:55 +0530

    creating an errorHandler middleware

--- backend/middlewares/errorHandler.middleware.js
+++ backend/middlewares/errorHandler.middleware.js
@@ -1,9 +1,17 @@
 const errorHandler = async (error, req, res, next) => {
     try {
+        let err = { ...error }
 
+        err.message = error.message || "Something went wrong!"
+        console.log(err.message);
+
+        res.status(err.statusCode || 500).json({
+            success: false,
+            message: err.message
+        })
     } catch (error) {
-
+        next(error);
     }
 }
 
-export default errorHandler;
+export default errorHandler;    
