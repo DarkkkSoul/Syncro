30f003752e713d7b0deeb233d43059407503cb6c
Mahesh Kumar G <maheshh.kumar1508@gmail.com>
2025-08-31 17:44:56 +0530

    join team + delete team + exit team + their routes

--- backend/controllers/team.controller.js
+++ backend/controllers/team.controller.js
@@ -35,3 +35,71 @@ export const createTeam = async (req, res, next) => {
         next(error);
     }
 }
+
+export const joinTeam = async (req, res, next) => {
+    try {
+
+        const teamId = req.params.id;
+        const user = req.user;
+
+        if (!teamId) {
+            const error = new Error('Team not found');
+            error.statusCode = 404;
+            throw error;
+        } else {
+            const team = await Team.findByIdAndUpdate(teamId, { $push: { members: [user._id] } });
+            await User.findByIdAndUpdate(user._id, { $push: { teams: [team._id] } });
+
+            res.status(201).json({
+                message: 'Joined to the team',
+                team
+            })
+        }
+
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const deleteTeam = async (req, res, next) => {
+    try {
+        const teamId = req.params.id;
+        const user = req.user;
+
+        const team = await Team.findById(teamId);
+
+        if (user._id == team.admin) {
+            await Team.deleteOne({ _id: teamId });
+            // here it is only updated to current user, not all the user, should come up with logic
+            // await User.findByIdAndUpdate(user._id, { $pull: { teams: teamId } });
+            res.status(201).json({
+                message: 'Team deleted',
+            })
+        } else {
+            const error = new Error('Not an admin');
+            error.statusCode = 404;
+            throw error;
+        }
+
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const exitTeam = async (req, res, next) => {
+    try {
+
+        const teamId = req.params.id;
+        const user = req.user;
+
+        await User.findByIdAndUpdate(user._id, { $pull: { teams: teamId } });
+        await Team.findByIdAndUpdate(teamId, { $pull: { members: user._id } });
+
+        res.status(201).json({
+            message: "Exited from the team"
+        })
+
+    } catch (error) {
+        next(error);
+    }
+}
--- backend/routes/team.route.js
+++ backend/routes/team.route.js
@@ -1,9 +1,12 @@
 import { Router } from 'express'
-import { createTeam } from '../controllers/team.controller.js'
+import { createTeam, deleteTeam, exitTeam, joinTeam } from '../controllers/team.controller.js'
 import authorizationMiddleware from '../middlewares/authorize.middleware.js';
 
 const teamRouter = Router();
 
 teamRouter.post('/create', authorizationMiddleware, createTeam);
+teamRouter.post('/join/:id', authorizationMiddleware, joinTeam);
+teamRouter.post('/delete/:id', authorizationMiddleware, deleteTeam);
+teamRouter.post('/exit/:id', authorizationMiddleware, exitTeam);
 
 export default teamRouter;
